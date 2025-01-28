87d0554be2bd7842240172ed8ed0021d0e437265
BibinVinoy <bibincv237@gmail.com>
2025-01-28 23:33:05 +0530

    refactor:get_object_data

--- zkteco_biometrics/zkteco_biometrics/zkteco/attendance.py
+++ zkteco_biometrics/zkteco_biometrics/zkteco/attendance.py
@@ -23,23 +23,3 @@ def get_attendance(ip:str, port=4370, timeout=30, device_id=None, clear_from_dev
     finally:
         if conn:
             conn.disconnect()
-
-
-@frappe.whitelist()
-def get_attendance_by_device(device_id, ip_address, clear_device_log:bool, port=4370, timeout=30):
-	# ip, port=4370, timeout=30, device_id=None, clear_from_device_on_fetch=False
-	data = get_attendance(
-			ip=ip_address,
-			port=4370,
-			timeout=timeout,
-			device_id=device_id,
-			clear_from_device_on_fetch=clear_device_log
-		)
-
-	log_doc = frappe.new_doc("ZKTeco")
-	log_doc.title = datetime.now()
-	log_doc.attendance = json.dumps(data.get("attendance")[0:5]) if data.get("attendance") else ""
-	log_doc.disable_device = str(data.get("disable_device")) if data.get("disable_device") else ""
-	log_doc.enable_device = str(data.get("enable_device")) if data.get("disable_device") else ""
-	log_doc.clear_device = str(data.get("clear_attendance")) if data.get("clear_attendance") else ""
-	log_doc.insert()
