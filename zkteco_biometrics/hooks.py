3c1c49f82e7e46270c467200a5670a1e26afe818
BibinVinoy <bibincv237@gmail.com>
2025-02-10 20:16:54 +0530

    feat:enque-get_attn_by_device

--- zkteco_biometrics/hooks.py
+++ zkteco_biometrics/hooks.py
@@ -151,7 +151,7 @@ app_license = "mit"
 scheduler_events = {
     "cron": {
         "0/5 * * * *": [
-            "zkteco_biometrics.zkteco_biometrics.zkteco.attendance.get_attendance_by_device"
+            "zkteco_biometrics.zkteco_biometrics.zkteco.attendance.get_device_attendance"
         ]
     }
 	# "all": [
